69ad5f5dfb78ac7fdcb6146aaf72b236257205b5
reubensrichardson <reubensrichardson@gmail.com>
2014-09-14 15:08:09 +1000

    added a test class

---
+++ LFTest.h
@@ -0,0 +1,13 @@
+//
+//  LFTest.h
+//  Git Test
+//
+//  Created by Cassandra Richardson on 14/09/2014.
+//  Copyright (c) 2014 luckyfin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LFTest : NSObject
+
+@end
---
+++ LFTest.m
@@ -0,0 +1,13 @@
+//
+//  LFTest.m
+//  Git Test
+//
+//  Created by Cassandra Richardson on 14/09/2014.
+//  Copyright (c) 2014 luckyfin. All rights reserved.
+//
+
+#import "LFTest.h"
+
+@implementation LFTest
+
+@end
